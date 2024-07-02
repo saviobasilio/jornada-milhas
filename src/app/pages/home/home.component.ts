758cfaa2803c15a7f6011496a9d24e965800a720
saviobasilio <savioeduardob@gmail.com>
2024-07-01 23:59:13 -0300

    feat: including API integration and taking the returned data and displaying in the component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { Promocao } from 'src/app/core/types/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public listaPromocoes!: Promocao[];
 
+  constructor(
+    private servicoPromocao: PromocaoService
+  ){
+
+  }
+
+  ngOnInit() :void{
+    this.servicoPromocao.listar().subscribe(
+      resposta => {
+        this.listaPromocoes = resposta;
+      }
+    );
+  }
 }

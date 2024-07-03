f588f8048cfa0a90d694472c6f8ea494ae7ea7b2
saviobasilio <savioeduardob@gmail.com>
2024-07-02 23:41:11 -0300

    feat: creating controller for modal

--- src/app/core/services/form-busca.service.ts
+++ src/app/core/services/form-busca.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MatChipSelectionChange } from '@angular/material/chips';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +11,12 @@ export class FormBuscaService {
 
   formBusca : FormGroup
 
-  constructor() {
+  constructor(private dialog: MatDialog, ) {
     this.formBusca = new FormGroup({
       somenteIda: new FormControl(false),
       origem: new FormControl(null),
-      destino: new FormControl(null)
+      destino: new FormControl(null),
+      tipo: new FormControl("Econômica")
     })
   }
 
@@ -22,5 +26,20 @@ export class FormBuscaService {
       throw new Error('FormControl com nome "${nome}" não existe.')
     }
     return control as FormControl;
+  }
+
+  alterarTipo(evento: MatChipSelectionChange, tipo: string)
+  {
+    if(evento.selected){
+      this.formBusca. patchValue({
+        tipo,
+      })
+    }
+  }
+
+  openDialog() {
+    this.dialog.open(ModalComponent, {
+      width: '50%'
+    });
   }
 }
